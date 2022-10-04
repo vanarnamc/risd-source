5e2ebf30426b98ff272307457f007f0c7b9c1a7d
clinton-van-arnam <97487772+clinton-van-arnam@users.noreply.github.com>
2022-10-04 13:10:46 -0400

    Class Update

--- project-1/drawer.js
+++ project-1/drawer.js
@@ -1,4 +1,13 @@
+/**
+ * @type HTMLCanvasElement
+ * 
+ */
+
+
 const canvasTag = document.querySelector("canvas")
+
+
+
 
 canvasTag.width = window.innerWidth * 2
 canvasTag.height = window.innerHeight * 2
@@ -14,8 +23,13 @@ let aimY = null
 let currentX = null
 let currentY = null
 
+
+
+
 let i = 0
-const images = ["./images/1.gif", "./images/2.gif", "./images/3.gif", "./images/4.gif", "./images/5.gif"].map(src => {
+let j =0;
+const images = ["./images/1.gif", "./images/2.gif", "./images/3.gif", "./images/4.gif", "./images/5.gif"]
+.map(src => {
   const image = document.createElement("img")
   image.src = src
   return image
@@ -33,15 +47,37 @@ document.addEventListener("mousemove", function (event) {
 
 canvasTag.addEventListener("click", function () {
   i = i + 1
+  currentX = currentX *(Math.random()*10)
+  currentY = currentY *(Math.random()*10)
   if (i >= images.length) {
     i = 0
   }
 })
 
 
+// timer code
+var tid = setTimeout(mycode, 20);
+function mycode() {
+  j=j+1;
+  // do some stuff...
+  tid = setTimeout(mycode, 20); // repeat myself
+ 
+  console.log(j);
+  if (j >= 5) {
+    j = 0
+  }
+}
+//
+context.fillStyle="#009578";
+
+function playVid() {
+  vid.play();
+}
+
+
 const draw = function () {
   if (currentX) {
-    if (images[i].complete) {
+    if (images[j].complete) {
     	context.drawImage(images[i], currentX - 200, currentY - 300, 400, 600)
   	}
     
@@ -51,6 +87,9 @@ const draw = function () {
   
   requestAnimationFrame(draw)
 }
+
+
+
 
 draw()
 
